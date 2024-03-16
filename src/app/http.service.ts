94422e4ed2ee9a02ef2c3a4a69c2cf45ee44eecd
arfgndgd <gundogduarif4@gmail.com>
2024-03-16 15:20:02 +0300

    list and form components added/designed

---
+++ src/app/http.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { IEmployee } from './interfaces/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  apiUrl="https://localhost:7108"
+  http = inject(HttpClient);
+  constructor() { }
+
+  getAllEmployee(){
+    return this.http.get<IEmployee[]>(this.apiUrl + "/api/Employee")
+  }
+}
---
+++ src/app/interfaces/employee.ts
@@ -0,0 +1,9 @@
+export interface IEmployee{
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    age: number;
+    salary: number;
+}
